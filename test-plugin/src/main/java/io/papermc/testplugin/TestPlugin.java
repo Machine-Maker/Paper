89a8ed877de7b1decacd4932884ad993b93c6def
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-03-25 10:25:20 -0700

    Add RecipeCraftEvent - WIP

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,6 +1,12 @@
 package io.papermc.testplugin;
 
+import io.papermc.paper.event.inventory.RecipeCraftEvent;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
@@ -8,5 +14,12 @@ public final class TestPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onEvent(RecipeCraftEvent event) {
+        event.setResult(new ItemStack(Material.STICK, 1));
+        System.out.println(event.getResult());
+        System.out.println(event.getRecipe());
     }
 }
